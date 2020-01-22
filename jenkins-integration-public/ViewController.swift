dfaf3db9463c0f28c305c9e12c58e0545f796533
Vladimir Jovicevic <jovicevicv@gmail.com>
2020-01-22 11:00:38 +0100

    Added fastlane

---
+++ jenkins-integration-public/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jenkins-integration-public
+//
+//  Created by admin on 1/16/20.
+//  Copyright © 2020 violetsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
